1dbe3020265ec4279404418e67d6289580639d96
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-08-16 20:23:45 +0300

    added test

--- python/code_challenges/trees/tests/test_trees.py
+++ python/code_challenges/trees/tests/test_trees.py
@@ -40,3 +40,16 @@ def test_max_value():
     tree.root.right = Node(2)
     tree.root.right.right = Node(1)
     assert tree.max_value() ==  96
+
+def test_breadthFirst_result():
+    tree = BinaryTree()
+    tree.root = Node(2)
+    tree.root.left = Node(7)
+    tree.root.right = Node(5)
+    tree.root.left.left = Node(2)
+    tree.root.left.right = Node(6)
+    tree.root.left.right.left = Node(5)
+    tree.root.left.right.right = Node(11)
+    tree.root.right.right = Node(9)
+    tree.root.right.right.left = Node(4)
+    assert breadth_first(tree) == [2, 7, 5, 2, 6, 9, 5, 11, 4]
