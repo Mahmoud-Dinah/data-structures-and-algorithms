878a75b37ea68c8d5b1b0d77c528717922e4daae
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-09-08 04:17:29 +0300

    added whiteboard & readme

--- python/code_challenges/hashmap-left-join/tests/test_hashmap_left_join.py
+++ python/code_challenges/hashmap-left-join/tests/test_hashmap_left_join.py
@@ -1,5 +1,14 @@
-from hashmap_left_join import __version__
+from hashmap_left_join.hashmap_left_join import *
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_happey_path():
+    hash_one = HsahTable()
+    hash_one.add('test', 'roll')
+    hash_one.add('qwe', 'in')
+    hash_one.add('test', 'in')
+    hash_one.add('color', 'red')
+    hash_two = HsahTable()
+    hash_two.add('color', 'red')
+    hash_two.add('in', 'roll')
+    hash_two.add('in', 'in')
+    assert (left_join(hash_one,hash_two)) == [['test', 'in', None], ['qwe', 'in', None], ['color', 'red', 'red']]
